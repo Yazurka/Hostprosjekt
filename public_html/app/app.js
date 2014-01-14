8587f389e54d4b31dac5dccfade4641ca3443bd1
Rune <r.aasvestad@live.com>
2014-01-14 12:55:29 +0100

    Småfix av bootstrap eksempler, og endret litt kontroller struktur.

--- public_html/app/app.js
+++ public_html/app/app.js
@@ -4,7 +4,7 @@ dashboardApp.config(function($routeProvider) {
     $routeProvider
             .when('/',
             {
-                controller: 'randomController',
+                controller: 'dashController',
                 templateUrl: 'app/partials/dashboard.html'
             })
             .when('/tables',
@@ -25,20 +25,59 @@ dashboardApp.config(function($routeProvider) {
             .otherwise({redirectTo: '/'});
 });
 
-dashboardApp.controller('randomController', function($scope, factory) {
-    $scope.number = [];
+
+
+dashboardApp.controller('dashController', function($scope, factory) {
+    $scope.number = [];//[{number:19}, {number:12},{number:124},{number:32}];
+    var data = {};
+    data.newHits = 22;
+    data.flight = 32;
+    data.returning = 44;
+    data.pageNavigations = 2;
+    $scope.data = data;
     init();
-    function init(){
+
+    function init() {
         $scope.number = factory.getNumber();
+
+    }
+    $scope.addNumber = function() {
+
+        $scope.number.push({number: $scope.div.number});
     }
 });
 
-dashboardApp.factory('factory', function(){
-    var number =   [{number:19}, {number:12},{number:124},{number:32}];
+dashboardApp.controller('randomController', function($scope, factory) {
+    $scope.number = [];//[{number:19}, {number:12},{number:124},{number:32}];
+
+    init();
+
+    function init() {
+        $scope.number = factory.getNumber();
+
+    }
+    $scope.addNumber = function() {
+
+        $scope.number.push({number: $scope.div.number});
+    }
+});
+
+dashboardApp.factory('factory', function() {
+//[{number:19}, {number:12},{number:124},{number:32}];
+    var number = getStuffs();
+
     var factory = {};
-    factory.getNumber = function(){
+
+    factory.getNumber = function() {
         return number;
     };
+    factory.addNumber = function(number) {
+        number.push(number);
+    }
     return factory;
 });
 
+function numbers($scope) {
+    var num = getStuffs();
+    return num;
+}
