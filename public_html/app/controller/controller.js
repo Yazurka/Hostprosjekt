7ccace1f56605b7e27647e0bd6171638b0ba0356
Rune <r.aasvestad@live.com>
2014-01-15 15:58:08 +0100

    testside

--- public_html/app/controller/controller.js
+++ public_html/app/controller/controller.js
@@ -17,20 +17,12 @@ dashboardApp.controller('dashController', function($scope, factory) {
     $scope.addNumber = function() {
 
         $scope.number.push({number: $scope.div.number});
-    }
+    };
 });
 
-dashboardApp.controller('randomController', function($scope, factory) {
-    init();
-
-    function init() {
-        $scope.number = factory.getNumber();
-
-    }
-    $scope.addNumber = function() {
-
-        $scope.number.push({number: $scope.div.number});
-    }
+dashboardApp.controller('randomController', function($scope) {
+    $scope.numbers = [{browser:"IE", number:12},{browser:"Netscape", number:22},{browser:"Chrome", number:45},{browser:"Firefox", number:87}]
+    //makeApiCall();
 });
 
 dashboardApp.factory('factory', function() {
